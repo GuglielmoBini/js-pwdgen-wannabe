ada3a9fe18b59bc8864ec71aac0343566c092faa
GuglielmoBini <guglielmobini@gmail.com>
2022-12-02 16:03:27 +0100

    aggiunta traccia e step

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*TRACCIA: Chiedi all'utente il suo nome, poi chiedi il suo cognome, poi chiedi il colore preferito, infine scrivi sulla pagina nomecognomecolorepreferito21.*/
+
+/*
+STEP:
+1. prendo l'elemento dal DOM
+
+2. preparo una variabile per il numero 21
+
+3. chiedo all'utente il suo nome
+3b. salvo il nome in una variabile
+
+4. chiedo all'utente il suo cognome
+4b. salvo il suo cognome in una variabile
+
+5. chiedo all'utente il suo colore preferito
+5b. salvo il suo colore preferito in una variabile
+
+6. inserisco il risultato nel DOM
+*/
