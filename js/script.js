dd6c1d7dd7da650776d59872aed5b4ecca39945f
GuglielmoBini <guglielmobini@gmail.com>
2022-12-02 16:54:00 +0100

    creato script

--- js/script.js
+++ js/script.js
@@ -15,5 +15,36 @@ STEP:
 5. chiedo all'utente il suo colore preferito
 5b. salvo il suo colore preferito in una variabile
 
-6. inserisco il risultato nel DOM
+6. calcolo la password 
+
+7. inserisco il risultato nel DOM
 */
+
+//***ESERCIZIO***
+
+// Prendo l'elemento dal DOM
+
+const targetElement = document.getElementById("target");
+
+// preparo una variabile per il numero 21
+
+const userNumber = 21;
+
+// chiedo all'utente il suo nome e (3b) lo salvo in una variabile
+
+const userName = prompt("Come ti chiami?");
+
+// chiedo all'utente il suo cognome e (4b) e lo salvo in una variabile
+
+const userSurname = prompt("qual'è il tuo cognome?");
+
+// chiedo all'utente il suo colore preferito e (5b) lo salvo in una variabile
+
+const userColor = prompt("dimmi il tuo colore preferito");
+
+// calcolo la password
+
+const userPassword = userName + userSurname + userColor + userNumber;
+
+//inserisco il risultato nel DOM
+targetElement.innerText = `La tua password è: ${userPassword}`;
